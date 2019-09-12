31aa1b9f8707b56c2062a176e1285b78160fd6c6
jabir.hussain <jabir.hussain@emumba.com>
2019-09-12 16:42:56 +0500

    Initialize KPI Dashboard with CRA and setup eslint and prettier

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { cleanup, render } from "@testing-library/react";
+import React from "react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+  const { debug } = render(<App />);
+  debug();
 });
